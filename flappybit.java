ca4eec87e1056c613172df64c7bbd832ca0b09ea
unknown <jaydenlhwong@gmail.com>
2022-05-24 13:58:34 -0400

    first

---
+++ flappybit.java
@@ -0,0 +1,25 @@
+import java.awt.*;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import java.io.*;
+
+public class flappybit extends JPanel {
+
+    public static int difficulty=0;
+    
+
+    public flappybit(){
+        setPreferredSize(new Dimension(1200,800));
+		setBackground(new Color(100,255,255));
+    }
+
+    public static void main(String[] args) {
+		JFrame myFrame = new JFrame("Flappy bit");
+		flappybit myPanel=new flappybit();
+		myFrame.add(myPanel);
+		myFrame.pack();
+		myFrame.setVisible(true);
+	}
+}
