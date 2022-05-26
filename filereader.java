4846fa4c59bd90079f8d91fe176feb223d8be1fd
unknown <jaydenlhwong@gmail.com>
2022-05-26 14:23:18 -0400

    push score command

---
+++ filereader.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.io.*;
+public class filereader {
+    //update the file of high scores with a new score
+    public static void pushScores(int x,int y) throws IOException{
+        int[] scores = new int[3];
+        Scanner sc=new Scanner(new File("scores.txt"));
+        for(int i=0;i<3;i++){
+            if(!sc.hasNextInt()) scores[i]=0;
+            else scores[i]=sc.nextInt();
+        }
+        scores[x]=y;
+        sc.close();
+        PrintWriter p=new PrintWriter(new FileWriter("scores.txt"));
+        for(int i=0;i<3;i++){
+            p.println(scores[i]);
+        }
+        p.close();
+    }
+}
